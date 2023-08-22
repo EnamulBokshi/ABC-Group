86960c2cdba9ec246ad6e3b44b041a6384e91c50
Enamul Bokshi <133862506+EnamulBokshi@users.noreply.github.com>
2023-08-23 02:12:04 +0600

    some files modified and about-us page re-designed

---
+++ js/aboutUs.js
@@ -0,0 +1,23 @@
+const ab001 = document.querySelector("#ab001");
+const ab002 = document.querySelector("#ab002");
+const ab003 = document.querySelector("#ab003");
+const ab004 = document.querySelector("#ab004");
+const ab005 = document.querySelector("#ab005");
+const ab006 = document.querySelector("#ab006");
+const ab007 = document.querySelector("#ab007");
+const ab008 = document.querySelector("#ab008");
+
+function viewdata(e,id){
+    const lists = document.querySelectorAll("#aboutParent li");
+    for(let li of lists){
+        li.className = li.className.replace("activated","hover");
+    }
+    e.currentTarget.className += ' activated';
+    const items = [ab001,ab002,ab003,ab004,ab005,ab006,ab007,ab008];
+    items.forEach(item =>{
+        item.classList += ' d-none';
+    })
+    console.log(id);
+    const currentItem = document.getElementById(id);
+    currentItem.classList.remove("d-none");
+}
